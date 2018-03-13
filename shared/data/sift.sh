1b834c956dbadfacabcf46aec21ee8d615615097
Christopher Baker <me@christopherbaker.net>
2018-03-12 20:52:06 -0500

    Fix scripts.

--- shared/data/sift.sh
+++ shared/data/sift.sh
@@ -3,7 +3,6 @@
 THIS_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 THIS_NAME=$(basename "${BASH_SOURCE[0]%.*}")
 INSTALL_PATH=${THIS_PATH}/${THIS_NAME}
-mkdir -p ${INSTALL_PATH}
 
 BASE_URL=ftp://ftp.irisa.fr/local/texmex/corpus/
 FILENAME=sift.tar.gz
@@ -11,8 +10,11 @@ FILENAME=sift.tar.gz
 echo "Installing ${THIS_NAME} data ..."
 
 if ! [ -d ${INSTALL_PATH} ]; then
+  mkdir -p ${INSTALL_PATH}
+  pushd ${THIS_PATH}
   curl -LO --progress-bar ${BASE_URL}/${FILENAME}
   tar xvf ${FILENAME}
   rm ${FILENAME}
+  popd
 fi
 echo "✅ ${INSTALL_PATH}"
